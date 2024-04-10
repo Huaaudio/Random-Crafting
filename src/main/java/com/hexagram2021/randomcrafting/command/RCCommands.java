527bfd74faff52320c42dcc10f6ccd803c625d29
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2024-04-10 22:44:33 +0800

    Fabric 1.19

--- src/main/java/com/hexagram2021/randomcrafting/command/RCCommands.java
+++ src/main/java/com/hexagram2021/randomcrafting/command/RCCommands.java
@@ -3,7 +3,6 @@ package com.hexagram2021.randomcrafting.command;
 import com.hexagram2021.randomcrafting.config.RCServerConfig;
 import com.hexagram2021.randomcrafting.util.IMessUpRecipes;
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.LongArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
@@ -14,14 +13,8 @@ import net.minecraft.network.protocol.game.ClientboundUpdateRecipesPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
-import net.minecraftforge.event.RegisterCommandsEvent;
 
 public class RCCommands {
-	public static void registerCommands(RegisterCommandsEvent event) {
-		final CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
-		dispatcher.register(RCCommands.register());
-	}
-
 	public static LiteralArgumentBuilder<CommandSourceStack> register() {
 		return Commands.literal("rc").then(
 				Commands.literal("reshuffle").requires(stack -> stack.hasPermission(RCServerConfig.PERMISSION_LEVEL_RESHUFFLE.get()))
--- src/main/java/com/hexagram2021/randomcrafting/mixin/ItemStackMixin.java
+++ src/main/java/com/hexagram2021/randomcrafting/mixin/ItemStackMixin.java
@@ -1,10 +1,8 @@
 package com.hexagram2021.randomcrafting.mixin;
 
 import com.hexagram2021.randomcrafting.util.IMutableItemStack;
-import net.minecraft.core.Holder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -15,13 +13,9 @@ public abstract class ItemStackMixin implements IMutableItemStack {
 	@Mutable @Shadow @Final
 	private Item item;
 
-	@Mutable @Shadow @Final
-	private Holder.Reference<Item> delegate;
-
 	@Override
 	public void setItemAndCount(Item item, int count) {
 		this.item = item;
-		this.delegate = ForgeRegistries.ITEMS.getDelegateOrThrow(item);
 		((ItemStack)(Object) this).setCount(count);
 	}
 }
