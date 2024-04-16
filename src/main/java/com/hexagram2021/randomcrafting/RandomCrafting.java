4f2a323986b33d627c71ceb01e94724ca9d3b1f9
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2024-04-16 23:31:36 +0800

    1.19.3

--- src/main/java/com/hexagram2021/randomcrafting/RandomCrafting.java
+++ src/main/java/com/hexagram2021/randomcrafting/RandomCrafting.java
@@ -3,18 +3,18 @@ package com.hexagram2021.randomcrafting;
 import com.hexagram2021.randomcrafting.command.RCCommands;
 import com.hexagram2021.randomcrafting.config.RCCommonConfig;
 import com.hexagram2021.randomcrafting.config.RCServerConfig;
+import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class RandomCrafting implements ModInitializer {
     public static final String MODID = "randomcrafting";
 
     public RandomCrafting() {
-        ModLoadingContext.registerConfig(MODID, ModConfig.Type.SERVER, RCServerConfig.SPEC);
-        ModLoadingContext.registerConfig(MODID, ModConfig.Type.COMMON, RCCommonConfig.SPEC);
+        ForgeConfigRegistry.INSTANCE.register(MODID, ModConfig.Type.SERVER, RCServerConfig.SPEC);
+        ForgeConfigRegistry.INSTANCE.register(MODID, ModConfig.Type.COMMON, RCCommonConfig.SPEC);
     }
 
     @Override
