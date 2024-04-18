bb51300ac5561e4926c2f8b0cdf42c848f44e4e8
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2024-04-19 00:25:47 +0800

    1.20.2

--- src/main/java/com/hexagram2021/randomcrafting/RandomCrafting.java
+++ src/main/java/com/hexagram2021/randomcrafting/RandomCrafting.java
@@ -3,7 +3,7 @@ package com.hexagram2021.randomcrafting;
 import com.hexagram2021.randomcrafting.command.RCCommands;
 import com.hexagram2021.randomcrafting.config.RCCommonConfig;
 import com.hexagram2021.randomcrafting.config.RCServerConfig;
-import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
+import fuzs.forgeconfigapiport.fabric.api.forge.v4.ForgeConfigRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
